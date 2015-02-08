7d3d437283816c6b29778bf3d86c9925f6b33905
Dylan Frese <dmfrese@gmail.com>
2015-02-07 21:51:28 -0500

    Removed AtomBoard
    The functionality of AtomBoard is now implemented in Board. Board now
    includes argument checking to ensure that the only mutable spaces are
    Marks, as well as additional constructor argument checking.
    
    Signed-off-by: Dylan Frese <dmfrese@gmail.com>
    
    Signed-off-by: Dylan Frese <dmfrese@gmail.com>

--- core/board/Board.rb
+++ core/board/Board.rb
@@ -6,16 +6,18 @@ class Board
     attr_reader :width, :height
 
     def initialize(width=3, height=3, levels=1)
+        raise ArgumentError.new("Size must be positive") if width <= 0 || height <= 0
+        raise ArgumentError.new("Levels must be positive") if levels < 0
         @width = width
         @height = height
         @winner = nil
         if block_given?
             @subboards = Array.new(width * height) {|i| proc[i]}
         else
-            if levels > 1
+            if levels > 0
                 @subboards = Array.new(width * height) {Board.new(width, height, levels - 1)}
             else
-                @subboards = Array.new(width * height) {AtomBoard.new(width, height)}
+                @subboards = Array.new(width * height) {Mark::BLANK}
             end
         end
         @subboards.each {|board| board.parent = self}
@@ -26,6 +28,45 @@ class Board
             @subboards[x + y * width]
         else
             @subboards[x]
+        end
+    end
+
+    def coerce(x, y, z=nil)
+        if z
+            index = x + y * width
+            value = z
+        else
+            index = x
+            value = y
+        end
+        return [index, value]
+    end
+    
+    def set_check(index, value)
+        raise TypeError.new("#{value} is not a type of Mark!") unless value.is_a? Mark
+        raise ArgumentError.new("Index #{index} is out of bounds!") if index >= @subboards.length
+        raise ArgumentError.new("The subboard at index #{index} is not of type Mark, and is immutable.") unless @subboards[index].is_a? Mark
+    end
+
+    private :coerce
+    
+    def []=(x, y, z = nil)
+        index, value = coerce(x, y, z)
+        if value == nil
+            value = Mark::BLANK
+        end
+        set_check(index, value)
+        @subboards[index] = value
+    end
+
+    def fill(x, y, z = nil)
+        index, value = coerce(x, y, z)
+        if value == nil
+            value = Mark::BLANK
+        end
+        set_check(index, value)
+        if @subboards[index].blank?
+            @subboards[index] = value
         end
     end
 
@@ -42,5 +83,3 @@ class Board
         @subboards.to_s
     end
 end
-
-require_relative 'AtomBoard'
