a6b1bd2ec7bdb1c3c79bdfeeca607e1dd251d49b
Dylan Frese <dmfrese@gmail.com>
2015-04-13 04:17:34 -0400

    Added documentation to the Move class.
    The Move class is now fully documented.

--- src/core/game/Move.rb
+++ src/core/game/Move.rb
@@ -1,8 +1,29 @@
+# This class represents a move made, or to be made on a certain board.
+# Formally, this encapsulates a request to set a Mark at a certain location on
+# the specified board to the Mark of the given player.
+# @author Dylan Frese
 class Move
-    attr_reader :atom, :player, :location
 
-    def initialize(location, atom, player)
-        @atom = atom
+    # @return [AbstractBoard] the board on which this Move is to be made
+    attr_reader :board
+    # @return [Player] the player making the move
+    attr_reader :player
+    # @return [Integer] the 0-based index of the Mark to be changed
+    attr_reader :location
+
+    # Initialize a new Move with a loaction, player, and board. The location is
+    # the index of the Mark which is to be changed. The board is the board
+    # of which the Mark is a subboard, or, more simply, the board on which the
+    # move is made.
+    # 
+    # As of now, no input validation is done, and this class is a simple
+    # container.
+    # @param [Integer] location the 0-based index of the Mark which is to be
+    #   changed
+    # @param [AbstractBoard] board the board on which the move is made
+    # @param [Player] player the player making the move
+    def initialize(location, board, player)
+        @board = board
         @location = location
         @player = player
     end
