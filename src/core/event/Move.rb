7eece36d369eaa6f713a110f3c279626a77db650
Dylan Frese <dmfrese@gmail.com>
2015-04-16 06:13:05 -0400

    Update Move to use the same format as drill
    Move#board now is an array of integers, as in AbstractBoard#drill, such
    that root.drill(board) is the subboard. This is necessary because
    references are lost through serialization and it is an inefficient way
    to communicate a single move.

--- src/core/event/Move.rb
+++ src/core/event/Move.rb
@@ -6,7 +6,7 @@ require_relative 'Event'
 # @author Dylan Frese
 class Move < Event
 
-    # @return [AbstractBoard] the board on which this Move is to be made
+    # @return [Array<Integer>] the board on which this Move is to be made
     attr_reader :board
     # @return [Player] the player making the move
     attr_reader :player
@@ -16,13 +16,15 @@ class Move < Event
     # Initialize a new Move with a loaction, player, and board. The location is
     # the index of the Mark which is to be changed. The board is the board
     # of which the Mark is a subboard, or, more simply, the board on which the
-    # move is made.
+    # move is made. It is specified in the same way as AbstractBoard#drill,
+    # that is to say, root.drill(board) is the subboard, where root is the root
+    # board.
     # 
     # As of now, no input validation is done, and this class is a simple
     # container.
     # @param [Integer] location the 0-based index of the Mark which is to be
     #   changed
-    # @param [AbstractBoard] board the board on which the move is made
+    # @param [Array<Integer>] board the board on which the move is made
     # @param [Player] player the player making the move
     def initialize(location, board, player)
         super(:GAME_MOVE, 1)
